cfb674625f697bfc7612fd06b6605664290018f4
Scott K <sjkersey@aol.com>
2017-06-19 15:50:48 -0400

    put absolute paths in build script for Jenkins

--- test_build.sh
+++ test_build.sh
@@ -2,8 +2,7 @@
 # modify separators as needed ";' for Windows, ":" for Linux
 
 # compile:
-javac -d ~/dev/junit_tests/test_Junit_01/class -cp /home/scott/dev/junit_tests/test_Junit_01/class:/home/scott/dev/junit_tests/test_Junit_01/lib/junit-4.10.jar -sourcepath /home/scott/dev/junit_tests/test_Junit_01/src/ mysources/MyUnit.java mytests/MyUnitTest.java
+javac -d /home/scott/dev/junit_tests/test_Junit_01/class -cp /home/scott/dev/junit_tests/test_Junit_01/class:/home/scott/dev/junit_tests/test_Junit_01/lib/junit-4.10.jar -sourcepath /home/scott/dev/junit_tests/test_Junit_01/src/ src/mysources/MyUnit.java src/mytests/MyUnitTest.java
 
 # run:
-java -cp ~/dev/junit_tests/test_Junit_01/class:/home/scott/dev/junit_tests/test_Junit_01/lib/junit-4.10.jar org.junit.runner.JUnitCore mytests.MyUnitTest
-JUnit version 4.10
+java -cp /home/scott/dev/junit_tests/test_Junit_01/class:/home/scott/dev/junit_tests/test_Junit_01/lib/junit-4.10.jar org.junit.runner.JUnitCore mytests.MyUnitTest
