e5cf1771c3acc5207a0dce6d8b12245c40292a20
Scott K <sjkersey@aol.com>
2017-06-19 17:04:38 -0400

    added notes on setting up Jenkins in sudo visudo file to allow Jenkins to run certian scripts owned by user scott; also added note about a seemingly false permission error during Jenkins build, but then the JUNIT test still passes.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # test_Junit_01
 for testing Junit features
+
+
+--note that when Jenkins is building the project in CentOS7 Linux (under /var/lib/jenkins/workspace/test_JUNIT_01) , there seems to be a false/insignificant error when comipiling a class file ( but then the JUnit test still passes)
+
+console output:
+...
+[test_JUNIT_01] $ /bin/sh -xe /tmp/hudson796757299827552391.sh
++ /home/scott/dev/junit_tests/test_Junit_01/test_build.sh
+src/mysources/MyUnit.java:3: error: error while writing MyUnit: /home/scott/dev/junit_tests/test_Junit_01/class/mysources/MyUnit.class (Permission denied)
+public class MyUnit {
+       ^
+1 error
+JUnit version 4.10
+.
+Time: 0.006
+
+OK (1 test)
+
+Finished: SUCCESS
+...
--- test_build.sh
+++ test_build.sh
@@ -1,6 +1,21 @@
 # modify directories as needed
 # modify separators as needed ";' for Windows, ":" for Linux
 
+#to allow Jenkins to run this in Linux you will need to 'sudo visudo'
+#and add entries like (first one below works on specific scripts owned by scott)
+#---------------------
+
+#allows jenkins to rebuild gnuradio and others installed under /home/scott
+#jenkins ALL=(scott) NOPASSWD: /home/scott/for_jenkins/run_as_jenkins_gnuradio.sh, /home/scott/for_jenkins/test_Jenkins_trigger_gnuradio.sh, /home/scott/dev/junit_tests/test_Junit_01/test_build.sh
+
+# allows Jenkins all accesses
+#jenkins ALL=(ALL) NOPASSWD: ALL
+
+#allows Jenkins to build JUNIT tests in scott's home dir (seems to be overidden by above)
+#jenkins ALL=(scott) NOPASSWD: /home/scott/for_jenkins/ru/home/scott/dev/junit_tests/test_Junit_01/test_build.sh
+
+#-----------------------
+
 # compile:
 javac -d /home/scott/dev/junit_tests/test_Junit_01/class -cp /home/scott/dev/junit_tests/test_Junit_01/class:/home/scott/dev/junit_tests/test_Junit_01/lib/junit-4.10.jar -sourcepath /home/scott/dev/junit_tests/test_Junit_01/src/ src/mysources/MyUnit.java src/mytests/MyUnitTest.java
 
